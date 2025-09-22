e6d8cb19ed0e2c0fd5f0cb74e812ab2daef6a7ac
Deysox <deysox@laposte.net>
2025-09-22 09:57:59 +0200

    Complétion joueur

--- joueur.cpp
+++ joueur.cpp
@@ -1,3 +1,27 @@
 //
 // Created by barnab on 22/09/2025.
 //
+
+#include <string>
+#include "joueur.h"
+
+string Joueur::getNom() const {
+    return nom;
+}
+
+int Joueur::getScore() const {
+    return score;
+}
+
+void Joueur::setNom(string n) {
+    nom = n;
+}
+
+void Joueur::setScore(int s) {
+    if (score < 0)
+        throw;
+    score = s;
+}
+
+
+
--- joueur.h
+++ joueur.h
@@ -5,4 +5,22 @@
 #ifndef PIXIES_JOUEUR_H
 #define PIXIES_JOUEUR_H
 
+#include "grille.h"
+#include <string>
+
+class Joueur {
+    string nom;
+    Grille* plateau;
+    int score;
+public:
+    Joueur(string n, int s) : nom(n), score(s) { plateau = new Grille; if (score < 0) throw; }
+    Joueur(string n) : nom(n), score(0) { plateau = new Grille; }
+    Joueur(int s) : nom("Player"), score(s) { plateau = new Grille; if (score < 0) throw; }
+    Joueur() : nom("Player"), score(0) { plateau = new Grille; }
+    int getScore() const;
+    void setScore(int s);
+    string getNom() const;
+    void setNom(string n);
+};
+
 #endif //PIXIES_JOUEUR_H
